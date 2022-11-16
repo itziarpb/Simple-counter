8f299946d3b1719f178e832bb79207b66a4744b0
itziarpb <itziar.perezb@gmail.com>
2022-11-16 22:03:20 +0000

    cambio

--- src/js/component/counter.js
+++ src/js/component/counter.js
@@ -3,21 +3,14 @@ import PropTypes from 'prop-types';
 
 const Counter = (props) => {
 	return (
-		<div>
-			<div className="bigCounter">
-				<div><i className="fa-regular fa-clock-nine"></i>reloj</div>
-				<div>{props.fourth}</div>
-				<div>{props.third}</div>
-				<div>{props.second}</div>
-				<div>{props.first}</div>
-			</div>
-			<div className="buttons">
-				<button type="button" className="btn btn-danger">Parar</button>
-				<button type="button" className="btn btn-warning">Reanudar</button>
-				<button type="button" className="btn btn-success">Reiniciar</button>
-			</div>
-		</div>);
-};
+		<div className="bigCounter">
+			<div><i className="fa-regular fa-clock-nine"></i></div>
+			<div>{props.fourth}</div>
+			<div>{props.third}</div>
+			<div>{props.second}</div>
+			<div>{props.first}</div>
+		</div>
+	)};
 
 Counter.propTypes = {
 	first: PropTypes.number,
--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,16 @@ import Counter from "./component/counter";
 import "../styles/index.css";
 
 let seconds=0;
+let one=0;
+let two=0;
+let three=0;
+let four=0;
+
 setInterval (()=>{
-	const one= Math.floor(seconds%10);
-    const two= Math.floor((seconds/10)%10);
-    const three= Math.floor((seconds/100)%10);
-    const four= Math.floor((seconds/1000)%10);
+	one= Math.floor(seconds%10);
+    two= Math.floor((seconds/10)%10);
+    three= Math.floor((seconds/100)%10);
+    four= Math.floor((seconds/1000)%10);
 	seconds++;
 ReactDOM.render(<Counter
                     first={one} 
@@ -18,5 +23,6 @@ ReactDOM.render(<Counter
 					fourth={four}
 />, document.querySelector("#app"));
 },1000);
+
 
 
